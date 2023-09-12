092ca171a16c54827715b3ca81c6a76fbcea84ca
crumplecup <erik.w.rose@gmail.com>
2023-09-11 20:07:15 -0700

    static folder

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -3,7 +3,7 @@ use api_lib::health::check;
 use api_lib::state::AppState;
 use axum::{routing::get, Router};
 use shuttle_runtime::CustomError;
-use shuttle_secrets::SecretStore;
+// use shuttle_secrets::SecretStore;
 use sqlx::{Executor, PgPool};
 use std::sync::Arc;
 use tower_http::services::ServeDir;
@@ -15,15 +15,15 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn axum(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: std::path::PathBuf,
+    // #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_static_folder::StaticFolder] static_folder: std::path::PathBuf,
     #[shuttle_shared_db::Postgres] pool: PgPool,
 ) -> shuttle_axum::ShuttleAxum {
-    let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
-        pass
-    } else {
-        return Err(anyhow!("Password was not found").into());
-    };
+    // let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
+    //     pass
+    // } else {
+    //     return Err(anyhow!("Password was not found").into());
+    // };
     pool.execute(include_str!("../schema.sql"))
         .await
         .map_err(CustomError::new)?;
